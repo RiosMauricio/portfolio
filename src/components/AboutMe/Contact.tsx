f861a9a22f71fac1099f8fc486bf486ad10ef906
Rios Mauricio <riosmaurii@gmail.com>
2025-04-21 02:30:20 -0300

    fix contact redirect

--- src/components/AboutMe/Contact.tsx
+++ src/components/AboutMe/Contact.tsx
@@ -6,7 +6,7 @@ export const Contact = () => {
       <Grid templateColumns={{ base: "1fr", md: "repeat(3, 1fr)" }} gap={4}>
         <GridItem>
           <a href="mailto:riosmaurii@gmail.com">
-            <Box display={'flex'} justifyContent={'center'} alignItems={'center'} flexDirection={'column'} borderRadius={5} _hover={{backgroundColor: 'blue.900'}}>
+            <Box display={'flex'} justifyContent={'center'} alignItems={'center'} flexDirection={'column'} borderRadius={5} _hover={{ backgroundColor: 'blue.900' }}>
               <Image mt={3} src="/assets/gmailIcon.jpg" h={'18vh'} />
               <Link mt={4} mb={2} href="mail.to:riosmauri@gmail.com">riosmaurii@gmail.com</Link>
             </Box>
@@ -15,17 +15,17 @@ export const Contact = () => {
         </GridItem>
 
         <GridItem>
-          <a href="https://www.linkedin.com/in/rios-mauricio-4504532b9">
-            <Box display={'flex'} justifyContent={'center'} alignItems={'center'} flexDirection={'column'} borderRadius={5} _hover={{backgroundColor: 'blue.900'}}>
-              <Image mt={3} src="/assets/likedinIcon.png" h={'18vh'}/>
+          <a href="https://www.linkedin.com/in/rios-mauricio-4504532b9" target="_blank">
+            <Box display="flex" justifyContent="center" alignItems="center" flexDirection="column" borderRadius={5} _hover={{ backgroundColor: "blue.900" }}>
+              <Image mt={3} src="/assets/likedinIcon.png" h="18vh" />
               <Link mt={4} mb={2}>Rios Mauricio</Link>
             </Box>
           </a>
         </GridItem>
 
         <GridItem>
-          <a href="https://github.com/RiosMauricio">
-            <Box display={'flex'} justifyContent={'center'} alignItems={'center'} flexDirection={'column'} borderRadius={5} _hover={{backgroundColor: 'blue.900'}}>
+          <a href="https://github.com/RiosMauricio" target="_blank">
+            <Box display={'flex'} justifyContent={'center'} alignItems={'center'} flexDirection={'column'} borderRadius={5} _hover={{ backgroundColor: 'blue.900' }}>
               <Image mt={3} src="/assets/githubIcon.png" h={'18vh'} />
               <Link mt={4} mb={2}>Rios Mauricio</Link>
             </Box>
